a0b283e43b99d29d1d1af9b0c0d83da18aede626
Naveen <navinkumarachari@gmail.com>
2020-01-25 22:02:31 +0530

    Make Lineage Great Again
    Change-Id: I1bad22c847187778e2473c363974b7167beaf358

--- core/config.mk
+++ core/config.mk
@@ -234,9 +234,9 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
-endif
+#ifneq ($(LINEAGE_BUILD),)
+include vendor/plato/config/BoardConfigLineage.mk
+#endif
 
 # The build system exposes several variables for where to find the kernel
 # headers:
